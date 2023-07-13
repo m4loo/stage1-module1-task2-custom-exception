2098b647c311c2b0af81a39ac2738c2688e01bca
m4loo <128167781+m4loo@users.noreply.github.com>
2023-07-13 14:32:22 +0600

    done

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,8 +5,12 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws IllegalArgumentException {
+    Student student = Student.getValueOf(studentID);
+    if (student == null) {
+      throw new StudentNotFoundException(studentID);
+    }
+    return student;
   }
 
   public static void main(String[] args) {
@@ -16,6 +20,5 @@ public class StudentManager {
       Student student = manager.find(IDs[i]);
       System.out.println("Student name " + student.getName());
     }
-
   }
 }
